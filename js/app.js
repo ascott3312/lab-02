dc49222dfddaa766bef5d14076f0d4949814acf1
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-07-23 17:02:38 -0500

    Dropdown Complete

--- js/app.js
+++ js/app.js
@@ -9,18 +9,24 @@ $(() => {
         Creature.all.push(new Creature(creature));
       });
     })
-    .then(() => {
+    .then(() => { 
       renderCreature();
+      fillKeywordDropdown();
     });
 });
 function Creature(creature) {
     this.imgURL = creature.image_url;
     this.getTitle = creature.title;
     this.getDescription= creature.description;
+    this.keyword = creature.keyword;
+    if(Creature.keyword.indexOf(this.keyword) < 0) {
+      Creature.keyword.push(this.keyword);
+    }
+
 }
 
 Creature.all = [];
-
+Creature.keyword = [];
 Creature.prototype.render = function () {
   const templateHTML = $('#photo-template').html();
   const renderedHTML = Mustache.render(templateHTML, this);
@@ -34,12 +40,18 @@ function renderCreature() {
     $('.photo-template').remove();
 }
 function fillKeywordDropdown(){
-  var keywordList = document.getElementById('keyword-select')
-  
-  for(var i = 0; i < all.length;i++){
-    var keyword = document.createElement('option');
-    keyword.textContent = all.[i].keyword;
-    keywordList.appendChild(keyword);
-  }
+  Creature.keyword.forEach(keyword => {
+    const $option = $('<option>').text(keyword).attr('value', keyword);
+    $('#keyword-select').append($option);
+  });
 }
-fillKeywordDropdown();
+// function fillKeywordDropdown(){
+//   var keywordList = document.getElementById('keyword-select')
+  
+//   for(var i = 0; i < all.length;i++){
+//     var keyword = document.createElement('option');
+//     keyword.textContent = all.[i].keyword;
+//     keywordList.appendChild(keyword);
+//   }
+// }
+// fillKeywordDropdown();
